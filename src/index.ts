827c708f684603ea99b9e8882a6e8b82125b2aeb
rooberttdev <robertbarbosa.dev@gmail.com>
2026-03-09 09:17:46 -0400

    feat: update host

--- src/index.ts
+++ src/index.ts
@@ -154,7 +154,7 @@ app.route({
 });
 
 try {
-  await app.listen({ port: env.PORT });
+  await app.listen({ host: "0.0.0.0", port: env.PORT });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
